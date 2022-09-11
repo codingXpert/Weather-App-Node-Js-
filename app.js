ed8325201ede638a9b8b87938b1bb674cad5ca4c
Vivek <vivekraj78965.vr@gmail.com>
2022-09-11 20:49:12 +0530

    linking bootStrap links

--- app.js
+++ app.js
@@ -1,21 +1,28 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 8000;
 
 
-app.get("/" , (re , res)=>{
+// public static path
+const static_path = path.join(__dirname , "./public");
+app.use(express.static(static_path));
+
+
+// routing  
+app.get("/" , (req , res)=>{
     res.send('Welcome to home page')
 });
 
-app.get("/about" , (re , res)=>{
+app.get("/about" , (req , res)=>{
     res.send('Welcome to about page')
 });
 
-app.get("/weather" , (re , res)=>{
+app.get("/weather" , (req , res)=>{
     res.send('Welcome to weather page')
 });
 
-app.get("*" , (re , res)=>{
+app.get("*" , (req , res)=>{
     res.send('404 page')
 });
 
