bb9ae8202d6db1f714b8cba3347b08927504a25d
Vivek <vivekraj78965.vr@gmail.com>
2022-09-20 22:13:35 +0530

    adding all hbs files

--- app.js
+++ app.js
@@ -1,29 +1,42 @@
 const express = require('express');
 const path = require('path');
+const hbs =  require('hbs');
 const app = express();
 const port = process.env.PORT || 8000;
 
 
 // public static path
 const static_path = path.join(__dirname , "./public");
+const templet_path = path.join(__dirname , "/templates/views");
+const partials_path = path.join(__dirname , "/templates/partials");
+
+app.set('view engine' , 'hbs');
+app.set('views',templet_path);
+
+hbs.registerPartials(partials_path);
 app.use(express.static(static_path));
+
+
+
 
 
 // routing  
 app.get("/" , (req , res)=>{
-    res.send('Welcome to home page')
+    res.render('index')
 });
 
 app.get("/about" , (req , res)=>{
-    res.send('Welcome to about page')
+    res.render('about')
 });
 
 app.get("/weather" , (req , res)=>{
-    res.send('Welcome to weather page')
+    res.render('weather')
 });
 
 app.get("*" , (req , res)=>{
-    res.send('404 page')
+    res.render('404error' , {
+        ErrorMsg:"Opps! Page Not Found"
+    })
 });
 
 
