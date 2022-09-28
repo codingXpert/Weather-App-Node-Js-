1e2700bf155a2b2c6eb9fcbcc2221dc2fda23632
Vivek <vivekraj78965.vr@gmail.com>
2022-09-28 08:50:11 +0530

    problem with form

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,10 @@
 const submitBtn = document.getElementById('submitBtn');
 
-const getInfo = () => {
+const getInfo = (event) => {
+    event.preventDefault();      // the problem with form is it tries to return
+                                // back into its original or initial form after
+                                // sending the data and refresh the page again 
+                                // so to prevent we need  to pass an argument(event) to function and use preventDefault()
     alert('hii');
 }
 
